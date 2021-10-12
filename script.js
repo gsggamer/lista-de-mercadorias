1d1c4ce353b3884921baf4b72fd58f7de095fff1
Gabriel Guimarães <gabriel.souza.guimaraes01@gmail.com>
2021-10-12 13:13:13 -0300

    Arquivos principais modificados para maior interatividade

---
+++ script.js
@@ -0,0 +1,13 @@
+var input = document.querySelector('input');
+var btn = document.querySelector('button');
+var list = document.querySelector('ul');
+var indice = 1;
+
+function add() {
+    var item = document.createElement('li');
+    
+    list.appendChild(item);
+    item.innerHTML = "<strong>Item " + indice + ":</strong> " + input.value;
+
+    indice++;
+}
